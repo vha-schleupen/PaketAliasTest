72a6e6a01aa5f26f1d28c4c0d54cfbfbfe51ab1f
Valentin Hamm <vha.gh@schleupen.de>
2022-02-23 11:00:03 +0100

    initial commit

---
+++ AliasTest/Program.cs
@@ -0,0 +1,12 @@
+﻿extern alias Json;
+
+namespace AliasTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Json.Newtonsoft.Json.JsonConvert.SerializeObject("Hello World!");
+        }
+    }
+}
